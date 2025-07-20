79bef45991127710b5ee9ed3fc9183a8dfc82312
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-20 03:02:32 -0300

    feat: create the main controller urlShortnerController to manage the incoming original url and make it shorter and save it in the DB. Also, in case of error, send to error middleware with next() expression

---
+++ api/controllers/urls.js
@@ -0,0 +1,29 @@
+const db = require('../db/db.js');
+const { randomBytes } = require('crypto');
+
+function urlShortnerController(req, res, next) {
+  const { url } = req.body;
+
+  if (!url) {
+    return next({ error: 'URL is required', statusCode: 400 });
+  }
+
+  const shortUrlBase = 'https://shorty.ly';
+  const randomId = randomBytes(10).toString('base64url').slice(0, 10);
+
+  console.log('randomId', randomId);
+
+  const stmt = db.prepare(`
+    INSERT INTO url (original_url, short_url)
+    VALUES (?, ?)`);
+
+  const { lastInsertRowid } = stmt.run(url, `${shortUrlBase}/${randomId}`);
+
+  const insertedRecord = db
+    .prepare('SELECT * FROM url WHERE id = ?')
+    .get(lastInsertRowid);
+
+  res.json(insertedRecord);
+}
+
+module.exports = urlShortnerController;
