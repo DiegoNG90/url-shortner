78948038753132f7ffc5fa31e39908e932d50c3f
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-25 19:02:31 -0300

    fix: on redirectToOriginalUrlByShortId controller by removing unnecesary nested if4

--- api/controllers/redirectToOriginalUrlByShortId.js
+++ api/controllers/redirectToOriginalUrlByShortId.js
@@ -12,12 +12,10 @@ function redirectToOriginalUrlByShortId(req, res, next) {
   const stmt = db.prepare('SELECT * FROM url WHERE id = ?').get(id);
 
   if (!stmt) {
-    if (!id) {
-      return next({
-        error: 'No record found with that id -- please try again',
-        statusCode: 404,
-      });
-    }
+    return next({
+      error: 'No record found with that id -- please try again',
+      statusCode: 404,
+    });
   }
 
   res.redirect(stmt.original_url);
